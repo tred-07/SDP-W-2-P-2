0e398fc8541236b6ba1ec26df89a1b77444d7da2
tred-07 <aranna.rtx.bd.02@gmail.com>
2024-12-16 03:35:21 +0600

    pause

--- control.js
+++ control.js
@@ -1,75 +1,40 @@
-const loadFood=()=>{
-    fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=a')
-    .then((res)=>res.json())
-    .then((res)=>{
-        console.log(res)
-        res['meals'].forEach(element => {
-            const parent=document.getElementById("all-food-container")
-           const div=document.createElement("div")
-           div.innerHTML=`
-           <div class="food-card">
+const loadFood = () => {
+  fetch("https://www.themealdb.com/api/json/v1/1/search.php?f=a")
+    .then((res) => res.json())
+    .then((res) => {
+      console.log(res);
+      res["meals"].forEach((element) => {
+        const parent = document.getElementById("all-food-container");
+        const div = document.createElement("div");
+        div.innerHTML = `
+           <div  class="food-card" onclick="foodDetails(${element.idMeal})">
+           <img src=${element.strMealThumb} title="Ok">
             <h1>${element.strMeal}</h1>
             <h3>${element.strCategory}</h3>
           </div>
-           `
-           parent.appendChild(div);
-        });
+           `;
+        parent.appendChild(div);
+      });
     })
-    .catch((err)=>console.log("Something went wrong"));
+    .catch((err) => console.log("Something went wrong"));
+};
 
-
-    fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=b')
-    .then((res)=>res.json())
-    .then((res)=>{
-        console.log(res)
-        res['meals'].forEach(element => {
-            const parent=document.getElementById("all-food-container")
-           const div=document.createElement("div")
-           div.innerHTML=`
-           <div class="food-card">
-            <h1>${element.strMeal}</h1>
-            <h3>${element.strCategory}</h3>
-          </div>
-           `
-           parent.appendChild(div)
-        });
+const foodDetails = (id) => {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
+    .then((res) => res.json())
+    .then((data) => {
+      data['meals'].forEach((el) => {
+        const parent = document.getElementById("selectedItem");
+        const div = document.createElement("div");
+        div.innerHTML = `
+        <h1>Ok</h1>
+        `;
+        console.log(el["strArea"],el['strCategory'],el['strInstructions'],el['strMeal'],el['strMealThumb'],el['strTags'],el['strYoutube'])
+        parent.appendChild(div);
+      });
     })
-    .catch((err)=>console.log("Something went wrong"))
-    fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=c')
-    .then((res)=>res.json())
-    .then((res)=>{
-        console.log(res)
-        res['meals'].forEach(element => {
-            const parent=document.getElementById("all-food-container")
-           const div=document.createElement("div")
-           div.innerHTML=`
-           <div class="food-card">
-            <h1>${element.strMeal}</h1>
-            <h3>${element.strCategory}</h3>
-          </div>
-           `
-           parent.appendChild(div)
-        });
-    })
-    .catch((err)=>console.log("Something went wrong"));
+    .catch((err) => console.log(err));
+};
 
-    fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=d')
-    .then((res)=>res.json())
-    .then((res)=>{
-        console.log(res)
-        res['meals'].forEach(element => {
-            const parent=document.getElementById("all-food-container")  
-           const div=document.createElement("div")
-           div.innerHTML=`
-           <div class="food-card">
-            <h1>${element.strMeal}</h1>
-            <h3>${element.strCategory}</h3>
-          </div>
-           `
-           parent.appendChild(div)
-        });
-    })
-    .catch((err)=>console.log("Something went wrong"))
-}
+loadFood();
 
-loadFood()
