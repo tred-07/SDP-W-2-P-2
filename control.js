1ea4522a956c34c81a7317f71a3429b5c9b45ac5
tred-07 <aranna.rtx.bd.02@gmail.com>
2024-12-16 23:05:05 +0600

    done

--- control.js
+++ control.js
@@ -44,38 +44,37 @@ const foodDetails = (id) => {
     .catch((err) => console.log(err));
 };
 
-const searchFoodDetails = (e1) => {
+document.getElementById("searchBtn").addEventListener("click", (e1) => {
   e1.preventDefault();
   const parent = document.getElementById("selectedItem");
-  const totalFood=document.getElementById("totalFood")
+  const totalFood = document.getElementById("totalFood");
   selectedItem.innerHTML = ``;
-  totalFood.innerHTML=``;
-  let key=document.getElementById("searchBox")
-  let pkey=key.innerText
-  console.log(pkey)
-  // fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${key}`)
-  //   .then((res) => res.json())
-  //   .then((data) => {
-  //     if (data["meals"].length == 0) {
-  //       div.innerHTML = `<h1>No data found.</h1>`;
-  //     } else {
-  //       totalFood.innerText=`${data["meals"].length} Found`
-  //       console.log(data['meals'].length)
-  //       // data["meals"].forEach((el) => {
-  //       //   const div = document.createElement("div");
-  //       //   div.innerHTML = `
-  //       // <img class="foodDetailsImg" src="${el["strMealThumb"]}">
-  //       // <h1>${el["strMeal"]}</h1> 
-  //       // <h1>${el["strCategory"]}</h1>
-  //       // <h1> ${el["strTags"]}</h1>
-  //       // <h3>${el["strIngredient1"]}, ${el["strIngredient2"]}, ${el["strIngredient3"]}, ${el["strIngredient4"]}, ${el["strIngredient5"]}, ${el["strIngredient6"]}, ${el["strIngredient7"]}, ${el["strIngredient8"]}</h3>
-  //       // <h3><a href="${el["strYoutube"]}">Click here</a></h3>
-  //       // `;
-  //       //   console.log(el["strArea"], el["strInstructions"], el["strYoutube"]);
-  //       //   parent.appendChild(div);
-  //       // });
-  //     }
-  //   })
-  //   .catch((err) => console.log(err));
-};
+  totalFood.innerHTML = ``;
+  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${document.getElementById("searchBox").value}`)
+    .then((res) => res.json())
+    .then((data) => {
+      if (data["meals"]== 'no data found') {
+        parent.innerHTML = `<h1>No data found.</h1>`;
+      } else {
+        totalFood.innerText=`${data["meals"].length} Found`
+        console.log(data['meals'].length,typeof(data),data)
+        data["meals"].forEach((el) => {
+          const div = document.createElement("div");
+          div.innerHTML = `
+        <img class="foodDetailsImg" src="${el["strMealThumb"]}">
+        <h1>${el["strMeal"]}</h1>
+        <h1>${el["strCategory"]}</h1>
+        <h1> ${el["strTags"]}</h1>
+        <h3>${el["strIngredient1"]}, ${el["strIngredient2"]}, ${el["strIngredient3"]}, ${el["strIngredient4"]}, ${el["strIngredient5"]}, ${el["strIngredient6"]}, ${el["strIngredient7"]}, ${el["strIngredient8"]}</h3>
+        <h3><a href="${el["strYoutube"]}">Click here</a></h3>
+        `;
+          console.log(el["strArea"], el["strInstructions"], el["strYoutube"]);
+          parent.appendChild(div);
+          div.classList.add("selectedItem")
+        });
+      }
+    })
+    .catch((err) => console.log(err));
+});
+
 loadFood();
